8b89adf7f2b0a6c02fb36dfee4856ea7574bd04a
Greg Karékinian <greg@karekinian.com>
2017-06-23 21:58:25 +0200

    Use zero mode, add Digital Ocean config, specify bootstrapped Chef version

--- .chef/knife.rb
+++ .chef/knife.rb
@@ -6,7 +6,12 @@ role_path                 "roles"
 environment_path          "environments"
 data_bag_path             "data_bags"
 encrypted_data_bag_secret "#{current_dir}/encrypted_data_bag_secret"
+local_mode                true # Chef local mode, replacing Solo
 
 cookbook_copyright     'remoteStorage'
 cookbook_license       'none'
 cookbook_email         'mail@remotestorage.io'
+
+knife[:bootstrap_version]   = '12.20.3'
+
+knife[:digital_ocean_access_token]  = "#{ENV['DIGITAL_OCEAN_ACCESS_TOKEN']}"
